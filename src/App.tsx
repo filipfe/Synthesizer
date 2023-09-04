9987086c98277f0ab8f810e8f42ecf2afe401b71
Filip <filfer05@gmail.com>
2023-09-04 15:54:25 +0200

    start button

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import useOpus from "./hooks/useOpus";
 import { OpusContext } from "./context/OpusContext";
 import Tempo from "./components/Tempo";
 import SoundControl from "./components/SoundControl";
+import { useState } from "react";
+import { start } from "tone";
 
 export default function App() {
+  const [audioStarted, setAudioStarted] = useState(false);
   const opus = useOpus();
-  return (
+
+  async function startAudio() {
+    await start();
+    setAudioStarted(true);
+  }
+
+  return audioStarted ? (
     <OpusContext.Provider value={opus}>
       <div className="flex flex-col gap-4 md:px-[12vw] py-[1in]">
         <Tempo />
@@ -15,5 +24,14 @@ export default function App() {
         </SoundControl>
       </div>
     </OpusContext.Provider>
+  ) : (
+    <div className="min-h-screen flex items-center justify-center">
+      <button
+        className="text-green-700 text-3xl px-6 py-3 bg-black transition-colors hover:bg-green-600 rounded-full hover:text-green-800"
+        onClick={startAudio}
+      >
+        Start
+      </button>
+    </div>
   );
 }
